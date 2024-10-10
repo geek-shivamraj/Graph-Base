49a05ecbf277f78f3a3bd832da9a25af6e34c0d6
Shivam-Omen <srvcode@gmail.com>
2024-10-10 08:44:41 +0530

    Adding Graph First try

---
+++ GraphFirstTry/src/main/java/com/geek/graph/GraphFirstTryApplication.java
@@ -0,0 +1,13 @@
+package com.geek.graph;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GraphFirstTryApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GraphFirstTryApplication.class, args);
+	}
+
+}
